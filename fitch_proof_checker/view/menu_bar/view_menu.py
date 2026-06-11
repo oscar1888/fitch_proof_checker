9fe7a86526ece4224ddf82b3022b99e7062a71e9
Oscar Mostafa Fath el bab <oscarx05@outlook.com>
2026-06-11 17:18:15 +0200

    View menu: refactor access to event loop

--- fitch_proof_checker/view/menu_bar/view_menu.py
+++ fitch_proof_checker/view/menu_bar/view_menu.py
@@ -1,15 +1,16 @@
+from PyQt6.QtWidgets import QApplication
 from fitch_proof_checker.view.style import MIN_SIZE, BASE_SIZE, MAX_SIZE, BASE_QLE_SIZE
 from fitch_proof_checker.view.utils import add_action
 
 
 def setup(fpe_main_window):
-    view_menu = fpe_main_window.menuBar().addMenu("&View")
+    view_menu = fpe_main_window.menuBar().addMenu("View")
     action_data = [
         ("Zoom In", apply_zoom(+1), "Ctrl++"),
         ("Zoom Out", apply_zoom(-1), "Ctrl+-"),
     ]
     for name, fun, shortcut in action_data:
-        add_action(fpe_main_window, view_menu, '&' + name, fun, shortcut)
+        add_action(fpe_main_window, view_menu, name, fun, shortcut)
 
 
 def apply_zoom(zoom):
@@ -17,10 +18,10 @@ def apply_zoom(zoom):
         if not (MIN_SIZE <= BASE_SIZE + fpe_main_window.zoom_level + zoom <= MAX_SIZE):
             return
         fpe_main_window.zoom_level += zoom
-        fpe_main_window.app.setStyleSheet(f"""
+        QApplication.instance().setStyleSheet(
+            f"""
             * {{ font-size: {BASE_SIZE + fpe_main_window.zoom_level}pt; }}
-            QLineEdit {{
-                font-size: {BASE_QLE_SIZE + fpe_main_window.zoom_level}pt;
-            }}
-        """)
+            QLineEdit {{font-size: {BASE_QLE_SIZE + fpe_main_window.zoom_level}pt; }}
+            """
+        )
     return _apply_zoom
