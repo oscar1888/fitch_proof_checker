abefa8232183f1423a650675cf3769c64278b794
Oscar Mostafa Fath el bab <oscarx05@outlook.com>
2026-06-12 04:06:09 +0200

    Change font for FPELineEdit and add Ctrl+E shortcut support

--- fitch_proof_checker/view/proof_layout/fpe_line_edit.py
+++ fitch_proof_checker/view/proof_layout/fpe_line_edit.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QLineEdit
+
 from fitch_proof_checker.view.style import BASE_QLE_SIZE
 
 
@@ -9,14 +10,18 @@ class FPELineEdit(QLineEdit):
         super().__init__()
         self.fpe_main_window = fpe_main_window
         self.proof_line = proof_line
-        font = QFont()
+        font = QFont("Cambria Math")
+        font.setItalic(True)
         font.setPointSize(BASE_QLE_SIZE)
         self.setFont(font)
 
     def keyPressEvent(self, event):
-        from fitch_proof_checker.view.proof_layout.proof_layout import add_step_after
+        from fitch_proof_checker.view.proof_layout.proof_layout import add_step_after, end_subproof
 
         if event.key() == Qt.Key.Key_A and (event.modifiers() & Qt.KeyboardModifier.ControlModifier):
             add_step_after(self.fpe_main_window)
+            return
+        if event.key() == Qt.Key.Key_E and (event.modifiers() & Qt.KeyboardModifier.ControlModifier):
+            end_subproof(self.fpe_main_window)
             return
         super().keyPressEvent(event)
