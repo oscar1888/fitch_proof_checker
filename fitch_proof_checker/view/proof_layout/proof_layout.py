07b79fd65180647385b340c904851b3f6081d7a3
Oscar Mostafa Fath el bab <oscarx05@outlook.com>
2026-06-12 04:11:18 +0200

    Proof menu: add subproofs functionalities

--- fitch_proof_checker/view/proof_layout/proof_layout.py
+++ fitch_proof_checker/view/proof_layout/proof_layout.py
@@ -25,7 +25,7 @@ def _get_premises(fpe_main_window):
     return [
         fpe_main_window.proof_layout.itemAt(i).widget()
         for i in range(fpe_main_window.proof_layout.count())
-        if fpe_main_window.proof_layout.itemAt(i).widget().is_premise
+        if (line := fpe_main_window.proof_layout.itemAt(i).widget()).is_assump and line.depth == 0
     ]
 
 
@@ -48,7 +48,7 @@ def add_premise(fpe_main_window):
     premises = _get_premises(fpe_main_window)
     last_premise = premises[-1] if premises else None
     idx = int(last_premise.line_number_label.text()) if last_premise else 0
-    row = ProofLine(fpe_main_window, is_premise=True)
+    row = ProofLine(fpe_main_window, is_assump=True)
     fpe_main_window.proof_layout.insertWidget(idx, row)
     row.formula_field.setFocus()
     update_layout(fpe_main_window)
@@ -58,12 +58,12 @@ def add_step_after(fpe_main_window):
     if not isinstance(QApplication.instance().focusWidget(), FPELineEdit):
         return
     current_line = QApplication.instance().focusWidget().proof_line
-    if current_line.is_premise:
+    if current_line.is_assump and current_line.depth == 0:
         last_premise = _get_premises(fpe_main_window)[-1]
         idx = int(last_premise.line_number_label.text())
     else:
         idx = int(current_line.line_number_label.text())
-    row = ProofLine(fpe_main_window)
+    row = ProofLine(fpe_main_window, depth=current_line.depth)
     fpe_main_window.proof_layout.insertWidget(idx, row)
     row.formula_field.setFocus()
     update_layout(fpe_main_window)
@@ -73,12 +73,16 @@ def add_step_before(fpe_main_window):
     if not isinstance(QApplication.instance().focusWidget(), FPELineEdit):
         return
     current_line = QApplication.instance().focusWidget().proof_line
-    if current_line.is_premise:
+    if current_line.is_assump and current_line.depth == 0:
         last_premise = _get_premises(fpe_main_window)[-1]
         idx = int(last_premise.line_number_label.text())
     else:
         idx = int(current_line.line_number_label.text()) - 1
-    row = ProofLine(fpe_main_window)
+    row = ProofLine(fpe_main_window,
+                    depth=current_line.depth - 1
+                    if current_line.is_assump and current_line.depth > 0
+                    else current_line.depth
+                    )
     fpe_main_window.proof_layout.insertWidget(idx, row)
     row.formula_field.setFocus()
     update_layout(fpe_main_window)
@@ -89,26 +93,88 @@ def delete_step(fpe_main_window):
         return
     current_line = QApplication.instance().focusWidget().proof_line
     premises = _get_premises(fpe_main_window)
-    if len(premises) == 1 and current_line.is_premise: return
+    if len(premises) == 1 and current_line.is_assump and current_line.depth == 0: return
 
-    lines = _get_all_lines(fpe_main_window)
-    curr_line_num = int(current_line.line_number_label.text())
-    fpe_main_window.proof_layout.removeWidget(current_line)
-    current_line.deleteLater()
-    fpe_main_window.proof_layout.itemAt(
-        curr_line_num - (1 if curr_line_num < len(lines) else 2)
-    ).widget().formula_field.setFocus()
+    if current_line.is_assump and current_line.depth > 0:
+        pos = int(current_line.line_number_label.text()) - 1
+        final_idx_to_del = pos
+        for line in [fpe_main_window.proof_layout.itemAt(i).widget() for i in
+                     range(pos, fpe_main_window.proof_layout.count())]:
+            if line.depth < current_line.depth or line.depth == current_line.depth and line.is_assump: break
+            final_idx_to_del += 1
+        for i in range(pos, final_idx_to_del + 1):
+            line = fpe_main_window.proof_layout.itemAt(i).widget()
+            fpe_main_window.proof_layout.removeWidget(line)
+            line.deleteLater()
+        # focus a quello dopo e se non c'è quello prima
+        fpe_main_window.proof_layout.itemAt(
+            pos if pos < fpe_main_window.proof_layout.count() else pos - 1
+        ).widget().formula_field.setFocus()
+    else:
+        lines = _get_all_lines(fpe_main_window)
+        curr_line_num = int(current_line.line_number_label.text())
+        fpe_main_window.proof_layout.removeWidget(current_line)
+        current_line.deleteLater()
+        fpe_main_window.proof_layout.itemAt(
+            curr_line_num - (1 if curr_line_num < len(lines) else 2)
+        ).widget().formula_field.setFocus()
     update_layout(fpe_main_window)
 
 
 def new_subproof(fpe_main_window):
-    # TODO: implement this function
-    pass
+    if not isinstance(QApplication.instance().focusWidget(), FPELineEdit):
+        return
+    current_line = QApplication.instance().focusWidget().proof_line
+
+    if current_line.is_assump and current_line.depth == 0:
+        glob_prems = _get_premises(fpe_main_window)
+        idx = int(glob_prems[-1].line_number_label.text())
+        depth = 1
+    elif not current_line.is_assump and not current_line.formula_field.text():
+        idx = int(current_line.line_number_label.text()) - 1
+        depth = current_line.depth + 1
+        fpe_main_window.proof_layout.removeWidget(current_line)
+        current_line.deleteLater()
+    else:
+        idx = int(current_line.line_number_label.text())
+        depth = current_line.depth + 1
+
+    row = ProofLine(fpe_main_window, is_assump=True, depth=depth)
+    fpe_main_window.proof_layout.insertWidget(idx, row)
+    row.formula_field.setFocus()
+    update_layout(fpe_main_window)
 
 
 def end_subproof(fpe_main_window):
-    # TODO: implement this function
-    pass
+    if not isinstance(QApplication.instance().focusWidget(), FPELineEdit):
+        return
+    current_line = QApplication.instance().focusWidget().proof_line
+    if current_line.depth == 0: return
+
+    lines = _get_all_lines(fpe_main_window)
+    pos = int(current_line.line_number_label.text()) - 1
+    next_line = fpe_main_window.proof_layout.itemAt(pos + 1).widget() if pos + 1 < len(lines) else None
+    last_idx = pos
+    for i in range(pos + 1, fpe_main_window.proof_layout.count()):
+        e = fpe_main_window.proof_layout.itemAt(i).widget()
+        if e.depth < current_line.depth or e.depth == current_line.depth and e.is_assump: break
+        last_idx = i
+
+    if (
+        current_line.is_assump
+        or (pos == len(lines) - 1
+            or next_line.depth < current_line.depth
+            or next_line.depth == current_line.depth and next_line.is_assump) and current_line.formula_field.text()
+    ):
+        row = ProofLine(fpe_main_window, depth=current_line.depth - 1)
+        fpe_main_window.proof_layout.insertWidget(last_idx + 1, row)
+        row.formula_field.setFocus()
+    else:
+        last_subproof_line = fpe_main_window.proof_layout.itemAt(last_idx).widget()
+        if last_subproof_line.depth > current_line.depth: return
+        last_subproof_line.depth -= 1
+        last_subproof_line.formula_field.setFocus()
+    update_layout(fpe_main_window)
 
 
 def edit_goal(fpe_main_window):
--- fitch_proof_checker/view/proof_layout/proof_line_view.py
+++ fitch_proof_checker/view/proof_layout/proof_line_view.py
@@ -6,25 +6,33 @@ from fitch_proof_checker.view.proof_layout.fpe_line_edit import FPELineEdit
 
 
 class ProofLine(QWidget):
-    def __init__(self, fpe_main_window, is_premise=False, depth=0):
+    def __init__(self, fpe_main_window, is_assump=False, depth=0):
         super().__init__()
         self.fpe_main_window = fpe_main_window
-        self.is_premise = is_premise
+        self.is_assump = is_assump
         self.depth = depth
+
+        self.OFFSET_FROM_NUM = 10
+        self.INTERBAR_SPACE = 14
+        self.TEXT_PADDING = 10
 
         line_layout = QHBoxLayout(self)
         line_layout.setContentsMargins(10, 5, 10, 5)
-        line_layout.setSpacing(15)
+        line_layout.setSpacing(5)
+
         self.line_number_label = QLabel("")
         self.line_number_label.setFixedWidth(25)
         line_layout.addWidget(self.line_number_label)
 
-        self.formula_field = FPELineEdit(self, self.fpe_main_window)
+        bars_space = self.OFFSET_FROM_NUM + (max(0, self.depth) * self.INTERBAR_SPACE) + self.TEXT_PADDING
+        line_layout.addSpacing(bars_space)
 
-        line_layout.addSpacing(10)
+        self.formula_field = FPELineEdit(self, self.fpe_main_window)
         line_layout.addWidget(self.formula_field, stretch=8)
+
         line_layout.addStretch()
-        if not self.is_premise:
+
+        if not self.is_assump:
             self.justification_field = FPELineEdit(self, self.fpe_main_window)
             line_layout.addWidget(self.justification_field, stretch=4)
 
@@ -32,18 +40,17 @@ class ProofLine(QWidget):
         from fitch_proof_checker.view.proof_layout.proof_layout import _get_premises
 
         painter = QPainter(self)
+        painter.setPen(QPen(Qt.GlobalColor.black, 2, cap=Qt.PenCapStyle.SquareCap))
 
-        pen = QPen(Qt.GlobalColor.black, 2)
-        pen.setCapStyle(Qt.PenCapStyle.SquareCap)
-        painter.setPen(pen)
+        start_x = self.line_number_label.geometry().right() + self.OFFSET_FROM_NUM
+        last_x = start_x
 
-        num_right = self.line_number_label.geometry().right()
-        formula_left = self.formula_field.geometry().left()
-        x_line = (num_right + formula_left) // 2
+        for i in range(self.depth + 1):
+            last_x = start_x + (i * self.INTERBAR_SPACE)
+            painter.drawLine(last_x, 15 if self.is_assump and i > 0 and i == self.depth else 0, last_x, self.height())
 
-        y_base = self.formula_field.geometry().bottom() + 5
+        glob_prem = _get_premises(self.fpe_main_window)
 
-        painter.drawLine(x_line, 0, x_line, y_base)
-
-        if self.is_premise and self == _get_premises(self.fpe_main_window)[-1]:
-            painter.drawLine(x_line, y_base, x_line + 100, y_base)
+        if self.is_assump and (self == glob_prem[-1] or int(self.line_number_label.text()) > len(glob_prem)):
+            y_base = self.formula_field.geometry().bottom() + 5
+            painter.drawLine(last_x, y_base, last_x + 75, y_base)
