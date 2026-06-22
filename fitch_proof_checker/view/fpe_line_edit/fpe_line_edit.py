042e64c426b80431ab581bc217713fdb085cede5
Oscar Mostafa Fath el bab <oscarx05@outlook.com>
2026-06-22 14:23:28 +0200

    Add logic symbols replacement

--- fitch_proof_checker/view/fpe_line_edit/fpe_line_edit.py
+++ fitch_proof_checker/view/fpe_line_edit/fpe_line_edit.py
@@ -1,7 +1,7 @@
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QLineEdit
 
-from fitch_proof_checker.view.utils.style import BASE_QLE_SIZE
+from fitch_proof_checker.view.utils.style import qle_font
 
 
 class FPELineEdit(QLineEdit):
@@ -9,15 +9,42 @@ class FPELineEdit(QLineEdit):
         super().__init__()
         self.fpe_main_window = fpe_main_window
         self.proof_line = proof_line
-        font = QFont("Cambria Math")
-        font.setItalic(True)
-        font.setPointSize(BASE_QLE_SIZE)
-        self.setFont(font)
-        self.textEdited.connect(self._handle_first_edit)
+        self.setFont(qle_font)
+        self.textEdited.connect(self._on_text_edited)
 
-    def _handle_first_edit(self, _):
+    def _on_text_edited(self, text):
         self.fpe_main_window.edited = True
-        self.textEdited.disconnect(self._handle_first_edit)
+        self._auto_replace_symbols(text)
+
+    def _auto_replace_symbols(self, text):
+        from fitch_proof_checker.view.fpe_line_edit.utils import symbol_map
+
+        new_text = text
+        for ascii_seq, (_, symbol) in symbol_map.items():
+            if ascii_seq in new_text:
+                new_text = new_text.replace(ascii_seq, symbol)
+
+        if new_text != text:
+            cursor_pos = self.cursorPosition()
+            diff = len(new_text) - len(text)
+
+            self.setText(new_text)
+            self.setCursorPosition(cursor_pos + diff)
+
+    def contextMenuEvent(self, event):
+        from fitch_proof_checker.view.fpe_line_edit.utils import symbol_map
+        menu = self.createStandardContextMenu()
+        menu.addSeparator()
+
+        logic_menu = menu.addMenu("Insert Logic Symbol")
+        symbols_to_add = [(f'{d} ({s})', s) for d, s in symbol_map.values()]
+
+        for name, char in symbols_to_add:
+            action = QAction(name, self)
+            action.triggered.connect(lambda checked, c=char: self.insert(c))
+            logic_menu.addAction(action)
+
+        menu.exec(event.globalPos())
 
     def keyPressEvent(self, event):
         from fitch_proof_checker.view.fpe_line_edit.utils import shortcuts_conds
--- fitch_proof_checker/view/fpe_line_edit/utils.py
+++ fitch_proof_checker/view/fpe_line_edit/utils.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication
 from fitch_proof_checker.view.fpe_line_edit.fpe_line_edit import FPELineEdit
-from fitch_proof_checker.view.proof_layout.proof_layout import add_step_after, end_subproof, _get_all_lines
+from fitch_proof_checker.view.proof_layout.proof_layout import add_step_after, end_subproof, get_all_lines
 
 
 def move(movement):
@@ -10,7 +10,7 @@ def move(movement):
             return
         current_line = QApplication.instance().focusWidget().proof_line
         pos = int(current_line.line_number_label.text()) - 1
-        lines = _get_all_lines(fpe_main_window)
+        lines = get_all_lines(fpe_main_window)
         if not (0 <= pos + movement < len(lines)): return
         line_to_focus = fpe_main_window.proof_layout.itemAt(pos + movement).widget()
         line_to_focus.formula_field.setFocus()
@@ -23,3 +23,16 @@ shortcuts_conds = [
     (lambda e: e.key() == Qt.Key.Key_Up, move(-1)),
     (lambda e: e.key() == Qt.Key.Key_Down, move(+1)),
 ]
+
+symbol_map = {
+    "<->": ('Co-Implies', "↔"),
+    "->": ('Implies', "→"),
+    "~": ('Not', "¬"),
+    "\\forall ": ('For all', "∀"),
+    "\\exists ": ('Exists', "∃"),
+    "/\\": ('And', "∧"),
+    "\\/": ('Or', "∨"),
+    "_|_": ('False', "⊥"),
+    "!=": ('Not Equal', "≠"),
+    "\\mul ": ('Times', "×")
+}
