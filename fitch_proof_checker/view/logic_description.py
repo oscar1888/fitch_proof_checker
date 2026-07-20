fb6af1f9c5af53466ddcfe04549e6b14a7670034
Oscar Mostafa Fath el bab <oscarx05@outlook.com>
2026-07-20 21:57:34 +0200

    Rename initial logic label

--- fitch_proof_checker/view/logic_description.py
+++ fitch_proof_checker/view/logic_description.py
@@ -7,7 +7,7 @@ def setup_logic_description(fpe_main_window):
     logic_title = QLabel("Logic:")
     logic_title.setStyleSheet("font-weight: bold;")
 
-    fpe_main_window.logic_label = QLabel("First Order Logic")
+    fpe_main_window.logic_label = QLabel("First-Order Logic (FOL)")
 
     logic_layout.addWidget(logic_title)
     logic_layout.addWidget(fpe_main_window.logic_label)
